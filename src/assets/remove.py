5d918b14ed285e20853285bdee1e2bfa6251414b
stromez <zemorts@gmail.com>
2023-04-22 00:13:55 +0700

    tu dien

--- src/assets/remove.py
+++ src/assets/remove.py
@@ -1,5 +1,28 @@
-with open("./src/assets/new.txt", "r") as input_file, open("output_2_words.txt", "w") as output_file:
-    for line in input_file:
-        words = line.strip().split()
-        if len(words) == 2:
-            output_file.write(line)
+# with open("./src/assets/output.txt", "r") as input_file, open("output.txt", "w") as output_file:
+#     lines = input_file.readlines()
+#     sorted_lines = sorted(lines)
+#     for line in sorted_lines:
+#         output_file.write(line)
+
+
+# with open("./src/assets/output.txt", "r") as input_file, open("output.txt", "w") as output_file:
+#     unique_lines = set(input_file)
+#     for line in unique_lines:
+#         output_file.write(line)
+        
+# with open("./src/assets/tudien.txt", "r") as input_file, open("output.txt", "w") as output_file:
+#     unique_lines = set()
+#     for line in input_file:
+#         # Loại bỏ khoảng trắng thừa
+#         line = line.strip()
+#         # Thêm vào tập hợp nếu chưa có
+#         if line not in unique_lines:
+#             unique_lines.add(line)
+#             output_file.write(line + "\n")
+       
+with open("./src/assets/output.txt", "r") as input_file, open("sorted_output.txt", "w") as output_file:
+    lines = input_file.readlines()
+    sorted_lines = sorted(lines)
+    for line in sorted_lines:
+        output_file.write(line)
+
